431f8e85a8d6a4c5b8c517caaec18dd4ff165de2
Victor Pinedo <victorpinedomartinez@gmail.com>
2023-09-12 14:17:00 -0500

    add the root route layout

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
 import './index.css'
+import React from 'react'
+import Root from './routes/root'
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+
+const router = createBrowserRouter([
+  { path: "/", element: <Root /> },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <h1>Hello World</h1>
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
