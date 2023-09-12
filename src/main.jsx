94a8a61877ed8dd8662e1d3aa60db1ef3c094a76
Victor Pinedo <victorpinedomartinez@gmail.com>
2023-09-12 14:40:58 -0500

    add new contact ui component

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,11 @@ import Root from './routes/root'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import ErrorPage from './ErrorPage'
+import Contact from "./routes/contact"
 
 const router = createBrowserRouter([
   { path: "/", element: <Root />, errorElement: <ErrorPage /> },
+  { path: "contacts/:contactId", element: <Contact /> },
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
