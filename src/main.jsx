d5fda8ac3153381d0a486d5c60cff8789b5cf100
Victor Pinedo <victorpinedomartinez@gmail.com>
2023-09-13 08:40:17 -0500

    add data loaders to url params

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,11 @@
 import './index.css'
 import React from 'react'
-import Root from './routes/root'
+import Root, { 
+  loader as rootLoader, 
+  action as rootAction,
+  loader as contactLoader
+
+} from './routes/root'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import ErrorPage from './ErrorPage'
@@ -11,8 +16,14 @@ const router = createBrowserRouter([
     path: "/", 
     element: <Root />, 
     errorElement: <ErrorPage />,
+    loader: rootLoader,
+    action: rootAction,
     children: [
-      { path: "contacts/:contactId", element: <Contact /> },
+      { 
+        path: "contacts/:contactId", 
+        element: <Contact />,
+        loader: contactLoader
+      },
     ] 
   },
 ]);
--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,6 +1,18 @@
-import { Link, Outlet } from "react-router-dom"
+import { Link, Outlet, useLoaderData, Form } from "react-router-dom"
+import { getContacts, createContact } from "../conctacts"
+
+export async function loader() {
+    const contacts = await getContacts();
+    return { contacts };
+}
+
+export async function action() {
+    const contact = await createContact();
+    return { contact };
+}
 
 export default function Root() {
+    const { contacts } = useLoaderData();
     return (
         <>
             <div id="sidebar">
@@ -25,24 +37,38 @@ export default function Root() {
                             aria-live="polite"
                         ></div>
                     </form>
-                    <form method="post">
+                    <Form method="post">
                         <button type="submit">New</button>
-                    </form>
+                    </Form>
                 </div>
-                
+
                 <nav>
-                    <ul>
-                        <li>
-                            <Link to={`/contacts/1`}>Your Name</Link>
-                        </li>
-                        <li>
-                            <Link to={`/contacts/2`}>Your Friend</Link>
-                        </li>
-                    </ul>
+                    {contacts.length ? (
+                        <ul>
+                            {contacts.map((contact) => (
+                                <li key={contact.id}>
+                                    <Link to={`contacts/${contact.id}`}>
+                                        {contact.first || contact.last ? (
+                                            <>
+                                                {contact.first} {contact.last}
+                                            </>
+                                        ) : (
+                                            <i>No Name</i>
+                                        )}{" "}
+                                        {contact.favorite && <span>★</span>}
+                                    </Link>
+                                </li>
+                            ))}
+                        </ul>
+                    ) : (
+                        <p>
+                            <i>No contacts</i>
+                        </p>
+                    )}
                 </nav>
             </div>
             <div id="detail">
-                <Outlet /> 
+                <Outlet />
             </div>
         </>
     );
